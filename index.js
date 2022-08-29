c176a0bc91e38da207b75b1f31f8e7a05c4855b7
Olga <o.smolyakova30@gmail.com>
2022-08-29 16:33:58 -0500

    added carausel

--- index.js
+++ index.js
@@ -14,3 +14,30 @@ items.forEach(item =>{
         })
     }
 })
+
+
+// -------------------
+
+const back = document.querySelector("#back");
+const next = document.querySelector("#next");
+const photos = ["src/1.jpg", "src/2.jpg", "src/3.jpg", "src/4.jpg", "src/5.jpg", "src/6.jpg", "src/7.jpg", "src/8.jpg",];
+console.log(photos);
+
+let i = 0;
+next.addEventListener('click', () => {
+i++;
+if (i>photos.length-1){
+    i=0;
+}
+document.querySelector("#pictures").src = photos[i]
+})
+
+back.addEventListener('click', () => {
+    i--;
+    if(i<0){
+        i=photos.length-1;
+    }
+    document.querySelector("#pictures").src = photos[i]
+})
+
+
